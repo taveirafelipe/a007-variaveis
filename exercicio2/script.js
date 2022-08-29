70b1082ed6018ab895c95e940f8738ba908c7937
taveirafelipe <taveira.felipe@gmail.com>
2022-08-29 15:25:25 -0300

    turma-ammal

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,16 +1,16 @@
 //Escreva um programa que faça 3 perguntas de Sim ou Não, e guarde cada resposta em uma variável.
-/*const r1 = prompt("Hoje esta quente?")
-const r2 = prompt("Choveu hoje?")
-const r3 = prompt("Voce esta feliz?")*/
+/*let r1 = prompt("Hoje esta quente?")
+let r2 = prompt("Choveu hoje?")
+let r3 = prompt("Voce esta feliz?")*/
 
 //Altere seu programa, para que o texto das perguntas também sejam guardadas em variáveis. 
 //Seu comando de **prompt** deve mudar um pouco, como mostrado abaixo:
-const p1 = "Hoje esta quente?"
-const r1 = prompt("Hoje esta quente?")
-const p2 = "Choveu hoje?"
-const r2 = prompt("Choveu hoje?")
-const p3 = "Voce esta feliz?"
-const r3 = prompt("Voce esta feliz?")
+let p1 = "Hoje esta quente?"
+let r1 = prompt("Hoje esta quente?")
+let p2 = "Choveu hoje?"
+let r2 = prompt("Choveu hoje?")
+let p3 = "Voce esta feliz?"
+let r3 = prompt("Voce esta feliz?")
 
 //Imprima na tela todas as respostas. Por exemplo:
 console.log (p1, r1)
